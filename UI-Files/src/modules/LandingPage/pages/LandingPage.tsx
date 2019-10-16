eb69f26c5cd7ad87df02ed9fa7cbabc8d0c57847
Julio Hurtado <julioahurtado@gmail.com>
2019-10-15 18:59:00 -0700

    Landing Pages initial set up

--- UI-Files/src/modules/LandingPage/pages/LandingPage.tsx
+++ UI-Files/src/modules/LandingPage/pages/LandingPage.tsx
@@ -1,6 +1,9 @@
 
 import * as React from 'react'
-import * as Router from 'react-router-dom'
+import  { Router, Link, Switch, Route} from 'react-router-dom'
+import history from '../../History/history'
+import VendorLandingPage from '../../Vendor/pages/VendorLandingPage'
+import CustomerLandingPage from '../../Customer/pages/CustomerLandingPage'
  
 interface LandingPageProps{
     description: String;
@@ -9,13 +12,31 @@ interface LandingPageProps{
 export default class LandingPage extends React.Component<LandingPageProps> {
     render(){
         return (
-            <div>
-                <h1>Choose your experience</h1>
-                <div>
-                    <button>Customer</button>
-                    <button>Vendor</button>
-                </div>
-            </div>
+            <Router history={history}>
+                <Switch>
+                    <Route exact={true} path='/'>
+                        <div>
+                            <h1>Choose your experience</h1>
+                            <div>
+                                <Link to={'/Customer'} >
+                                    <button>Customer</button>
+                                </Link>
+                                <Link to={'/Vendor'} >
+                                    <button>Vendor</button>
+                                </Link>
+                            </div>
+                        </div>
+                    </Route>
+                    <Route path='/Customer'>
+                        <CustomerLandingPage>
+                        </CustomerLandingPage>
+                    </Route>
+                    <Route path='/Vendor'>
+                        <VendorLandingPage>
+                        </VendorLandingPage>
+                    </Route>
+                </Switch>
+            </Router>
         )
     }
 }
