cd6f1297e7cb74a37b5596983d3ad51becc4d841
Bobby Mushkot <bobby.mushkot@gmail.com>
2019-10-21 23:06:49 -0700

    Add testData.sql, adds a row into Vendors and Customers tables.

---
+++ Database/sql/testData.sql
@@ -0,0 +1,5 @@
+INSERT INTO Customers() 
+	VALUES(123456789, "Robert", "Mushkot", "rmushkot@ucsc.edu", "password", 1115551234);
+
+INSERT INTO Vendors()
+	VALUES(987654321, "Cruz N Gourmet", "cruzngourmet@ucsc.edu", "drowssap", "American");
