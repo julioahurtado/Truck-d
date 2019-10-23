5c6ced9fdf9a6470f71beb5caa0aaaaa3251e56c
Mathew Scolari <scolarimathew@gmail.com>
2019-10-23 15:28:56 -0700

    Implemented reducer for vendor search

---
+++ UI-Files/src/Redux/ReducerFiles/VendorSearchReducer.ts
@@ -0,0 +1,49 @@
+import { VENDOR_SEARCH_BEGIN, VENDOR_SEARCH_SUCCESS, VENDOR_SEARCH_FAILURE } from '../ActionFiles/CustomerActions';
+import { VendorInfo } from '../InterfaceFiles/types'
+
+interface VendorSearchState {
+    vendors: VendorInfo[],
+    isLoading: Boolean,
+    error: Error
+};
+
+let initState: VendorSearchState = {
+    vendors: [null],
+    isLoading: false,
+    error: null
+};
+
+// TODO: need to create default state interface
+const VendorSearch = (state = initState, action) => {
+    switch(action.type) {
+
+        // Signals start of vendor-list fetch
+        case VENDOR_SEARCH_BEGIN:
+            return {
+                ...state,
+                isLoading: true
+            };
+
+        // Signals successfull fetch with vendors returned
+        case VENDOR_SEARCH_SUCCESS:
+            return {
+                ...state,
+                vendors: action.payload,
+                isLoading: false,
+            };
+
+        // Signals failed fetch for vendors with error message
+        case VENDOR_SEARCH_FAILURE:
+            return {
+                ...state,
+                vendors: null,
+                isLoading: false,
+                error: action.error
+            }
+
+        default:
+            return state
+    }
+}
+
+export default VendorSearch;
