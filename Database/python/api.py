d32622b9528dc07a1cbb50600b92c9621455cb98
Bobby Mushkot <bobby.mushkot@gmail.com>
2019-10-23 19:29:07 -0700

    Created create_vendor_user, connect_to_db, and disconnect_from_db methods

--- Database/python/api.py
+++ Database/python/api.py
@@ -11,6 +11,51 @@ app.config["DEBUG"] = True
 def home():
     return "home() function call."
 
+
+def create_vendor_user(restuarant, location, email, password, cuisine):
+    connection = connect_to_db()
+    dbCursor = connection.dbCursor()
+    customerID = random() # Random number for customerID
+    sql = ("""INSERT INTO Vendors"
+           VALUES (%d, %s, %s, %s, %s, %s)""")
+
+    data = (customerID, restuarant, location, email, password, cuisine)
+    try:
+        dbCursor.execute(sql, data)
+        connection.commit()
+    except Exception as e:
+        connection.rollback()
+        print(e)
+    finally:
+        dbCursor.close()
+        connection.disconnect_from_db()
+
+def connect_to_db()
+    try:
+        connection = mysql.connector.connect(
+            user = "admin", password = "truckdpassword",
+            host = "truckd.cy00g7ft3yfp.us-east-1.rds.amazonaws.com",
+            database = "Truckd", port = "3306"
+        )
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Username or password incorrect.\n")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist\n")
+        else:
+            print(err)
+        return None
+    return connection
+
+def disconnect_from_db(connection):
+    try:
+        connection.close()
+        return 1
+    except:
+        print("Disconnect Failure")
+        return -1
+
+
 # An example of what a api that would connect to a database looks like.
 """
 @app.route('/[example-route], methods=['GET'])
@@ -22,4 +67,3 @@ def insert_into_table(data):
     do_the_thing()
 
 """
-
