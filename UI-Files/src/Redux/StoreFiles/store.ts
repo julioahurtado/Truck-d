cbaae83085b20153f4df8a80f6a69c037a07149e
Mathew Scolari <scolarimathew@gmail.com>
2019-10-24 23:51:50 -0700

    Added Redux-thunk to project

--- UI-Files/src/Redux/StoreFiles/store.ts
+++ UI-Files/src/Redux/StoreFiles/store.ts
@@ -1,7 +1,9 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from "redux-thunk";
+
 import customer from '../ReducerFiles/CustomerReducer';
 import vendor from '../ReducerFiles/VendorReducer';
 
 // create root state and initialize store
 let rootReducer = combineReducers({customer, vendor});
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, applyMiddleware(thunk));
