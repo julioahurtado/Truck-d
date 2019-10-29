13db70787ef33094c49cd404ba16084a5b439f9a
Julio Hurtado <julioahurtado@gmail.com>
2019-10-28 23:40:00 -0700

    REST adapter. updated sampleflask to handle test

--- Database/python/sampleFlask.py
+++ Database/python/sampleFlask.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/",  methods=['GET'])
+@cross_origin()
+def getTest():
+    return "GET RECIEVED"
+
+@app.route("/",  methods=['POST'])
+@cross_origin()
+def postTest():
+    payload = request.get_json(force=True)
+    print(payload)
+    return "POST RECIEVED"
+
+@app.route("/",  methods=['OPTIONS'])
+@cross_origin()
+def preflightBypass():
+    return "OPTIONS RECIEVED"
+
+if __name__ == "__main__":
+    app.run(debug=True)
