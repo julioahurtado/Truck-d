614dc8cb0e30b296b8bbfcc4baf4f280185850ca
Julio Hurtado <julioahurtado@gmail.com>
2019-11-01 20:47:53 -0700

    demo functionality

--- Database/python/sampleFlask.py
+++ Database/python/sampleFlask.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
@@ -28,6 +28,47 @@ def postTest():
     payload = request.get_json(force=True)
     print('POST: ' + str(payload))
     return payload
+
+
+# Sign in demo code
+vendors = {
+    'a@gmail.com': 'abc123',
+    'b@gmail.com': 'password',
+    'c@gmail.com': 'cse115'
+}
+
+
+@app.route("/authenticatevendor",  methods=['POST'])
+@cross_origin()
+def authVendor():
+    payload = request.get_json(force=True)
+
+    if payload['email'] in vendors.keys() and vendors[payload['email']] == payload['password']:
+        resp = Response('Authenticated', 200)
+        return resp
+    else:
+        resp = Response('Not Authenticated', 401)
+        return resp
+
+    return Response('Server ERROR', 500)
+
+
+@app.route("/createVendorAccount",  methods=['POST'])
+@cross_origin()
+def createVendor():
+    payload = request.get_json(force=True)
+
+    if payload['email'] in vendors.keys():
+        resp = Response('User found with same email', 409)
+        return resp
+    else:
+        vendors[payload['email']] = payload['password']
+        resp = Response('User creation successful', 201)
+        print(vendors)
+        return resp
+
+    return Response('Server ERROR', 500)
+
 
 
 if __name__ == "__main__":
