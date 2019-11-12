379e34188bdbf72e0650af07996be0ca0eb481c2
Mathew Scolari <scolarimathew@gmail.com>
2019-11-12 10:43:21 -0800

    Finished designing state for menu page

--- UI-Files/src/Redux/ReducerFiles/CustomerReducer.ts
+++ UI-Files/src/Redux/ReducerFiles/CustomerReducer.ts
@@ -1,10 +1,13 @@
 import { combineReducers } from "redux";
 import { CustomerSearch, CustomerSearchState } from './CustomerReducers/CustomerSearchReducer';
+import { MenuPage, MenuPageState } from './CustomerReducers/MenuReducer';
 
 export interface CustomerState {
     search: CustomerSearchState
+    menuPage: MenuPageState
 }
 
 export const Customer = combineReducers<CustomerState>({
-    search: CustomerSearch
+    search: CustomerSearch,
+    menuPage: MenuPage
 });
---
+++ UI-Files/src/Redux/ReducerFiles/CustomerReducers/MenuReducer.ts
@@ -0,0 +1,79 @@
+import { GET_MENU_STATUS, GetMenuAction, UpdateMenuWithVendorAction, UPDATE_MENU_WITH_VENDOR } from '../../ActionFiles/CustomerActions';
+import { MenuItem, VendorInfo } from '../../InterfaceFiles/types'
+import { combineReducers } from 'redux';
+
+interface MenuState {
+    menu?: MenuItem[] | null,
+    isLoading: Boolean,
+    error?: Error | null
+}
+
+interface VendorState {
+    vendor?: VendorInfo | null
+};
+
+let vendorState: VendorState = {
+    vendor: null
+}
+
+let menuState: MenuState = {
+    menu: null,
+    isLoading: false,
+    error: null
+}
+
+export const Menu = (state = menuState, action: GetMenuAction): MenuState => {
+    switch(action.type) {
+
+        // Begin menu fetch
+        case GET_MENU_STATUS.BEGIN:
+            return {
+                ...state,
+                isLoading: true
+            };
+
+        // Menu successfully retrieved
+        case GET_MENU_STATUS.SUCCESS:
+            return {
+                ...state,
+                menu: action.payload,
+                isLoading: false,
+            };
+
+        // Menu retrieval failed
+        case GET_MENU_STATUS.FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.error
+            };
+
+        default:
+            return state
+    }
+};
+
+export const Vendor = (state = vendorState, action: UpdateMenuWithVendorAction): VendorState => {
+    switch(action.type) {
+
+        // Add vendor data to menu page
+        case UPDATE_MENU_WITH_VENDOR:
+            return {
+                ...state,
+                vendor: action.payload
+            };
+        
+        default: 
+            return state
+    }
+};
+
+export interface MenuPageState {
+    menu: MenuState,
+    vendor: VendorState
+}
+
+export const MenuPage = combineReducers<MenuPageState>({
+    menu: Menu,
+    vendor: Vendor
+});
