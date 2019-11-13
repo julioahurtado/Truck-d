5d9989a78ed7ab93399c87321b2b58cc9456ac9d
Julio Hurtado <julioahurtado@gmail.com>
2019-11-12 21:33:08 -0800

    container

--- UI-Files/src/modules/Customer/pages/CustomerMenuPage.tsx
+++ UI-Files/src/modules/Customer/pages/CustomerMenuPage.tsx
@@ -1,12 +1,38 @@
 
 import * as React from 'react'
+import { ListGroup } from 'react-bootstrap'
 
-export default class CustomerMenu extends React.Component<any> {
+interface CustomerMenuProps extends deleteWhenReduxIsAdded{
+    vendor?: any;
+    isLoading?: Boolean;
+
+}
+
+interface deleteWhenReduxIsAdded {
+    RestaurantName: String;
+    items: any[];
+}
+
+
+export default class CustomerMenu extends React.Component<CustomerMenuProps> {
    
     render() {
         return (
             <div>
                     <h1>{this.props.RestaurantName}</h1>
+                    <br/>
+                        {!this.props.isLoading && 
+                        <ListGroup style={{padding: '2px'}}>
+                            {this.props.items.map((item: any) => 
+                                {
+                                    return (
+                                        <div>
+
+                                        </div>
+                                    )
+                                })}
+                        </ListGroup>
+                            }
             </div>
         )
     }
