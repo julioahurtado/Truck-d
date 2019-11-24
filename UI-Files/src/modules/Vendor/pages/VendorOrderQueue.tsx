7ec2bebead636e6fbab448b57e0b4415a3d38947
Mathew Scolari <scolarimathew@gmail.com>
2019-11-23 16:20:34 -0800

    Implemented order queue and react components

--- UI-Files/src/modules/Vendor/pages/VendorOrderQueue.tsx
+++ UI-Files/src/modules/Vendor/pages/VendorOrderQueue.tsx
@@ -1,13 +1,71 @@
 
-import * as React from 'react'	
+import * as React from 'react'
+import { RootState } from '../../../Redux/StoreFiles/store';
+import { connect } from 'react-redux';
 
+import { OrdersState } from '../../../Redux/ReducerFiles/VendorReducers/OrdersReducer';
+import { fetchOrders } from '../../../Redux/ActionFiles/VendorActions';
+import VendorOrderQueueItem from '../components/VendorOrderQueueItem';
+import { Order } from '../../../Redux/InterfaceFiles/types';
 
-export default class VendorOrderQueue extends React.Component<any> {	
+interface VendorOrderQueueProps extends OrdersState, VendorOrderQueueDispatchProps{
+    id: number
+}
+
+interface VendorOrderQueueDispatchProps {
+    fetchOrders?: any
+};
+
+class OrderQueue extends React.Component<VendorOrderQueueProps> {
+    timer: any;
+
+    fetch_orders() {
+        this.props.fetchOrders(this.props.id);
+        this.timer = setTimeout(this.fetch_orders.bind(this), 10000);
+    } 
+
+    componentDidMount() {
+        this.fetch_orders()
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.timer)
+    }
+
     render(){	
-        return (	
-            <div>	
-                VendorOrderQueue	
-            </div>	
-        )	
-    }	
-} 
+        return (
+            <div>
+                <h1>Orders</h1>
+                {this.props.orders && this.props.orders.map((order: Order) => 
+                    {
+                        return (
+                            <VendorOrderQueueItem
+                                order={order}
+                            ></VendorOrderQueueItem>
+                        )
+                    }
+                )}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state: RootState): VendorOrderQueueProps => ({
+    id: state.vendor.profile.id,
+    orders: state.vendor.queue.orders,
+    isFetching: state.vendor.queue.isFetching,
+    isLoading: state.vendor.queue.isLoading,
+    error: state.vendor.queue.error
+});
+
+const mapDispatchToProps = (dispatch: any): VendorOrderQueueDispatchProps => ({
+    fetchOrders: (id: number) =>
+        dispatch(fetchOrders(id))
+});
+
+const VendorOrderQueue = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(OrderQueue)
+
+export default VendorOrderQueue;
