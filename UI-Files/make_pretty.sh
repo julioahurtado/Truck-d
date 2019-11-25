51a0828094024a8714da746c3098eea034cd2b1b
Julio Hurtado <julioahurtado@gmail.com>
2019-11-24 17:08:58 -0800

    remove shell file

--- UI-Files/make_pretty.sh
+++
@@ -1,8 +0,0 @@
-git checkout prettier-setup
-git pull
-git checkout react-redux-integration
-git checkout prettier-setup package.json
-npm install --global prettier
-npm install
-prettier --write "src/**/**/**/**.tsx"
-prettier --write "src/**/**/**/**.ts"
