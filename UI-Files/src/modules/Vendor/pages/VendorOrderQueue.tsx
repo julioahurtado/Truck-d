e192f63004a2f344ff4e38f5110a54d6c40655a0
Mathew Scolari <scolarimathew@gmail.com>
2019-12-02 13:19:00 -0800

    Fixed fetching bug when finishing an order

--- UI-Files/src/modules/Vendor/pages/VendorOrderQueue.tsx
+++ UI-Files/src/modules/Vendor/pages/VendorOrderQueue.tsx
@@ -20,8 +20,12 @@ interface VendorOrderQueueDispatchProps {
 class OrderQueue extends React.Component<VendorOrderQueueProps> {
   timer: any;
   fetch_orders() {
-    this.props.fetchOrders(this.props.id);
-    this.timer = setTimeout(this.fetch_orders.bind(this), 10000);
+    if (this.props.isLoading) {
+      setTimeout(this.fetch_orders, 1000);
+    } else {
+      this.props.fetchOrders(this.props.id);
+      this.timer = setTimeout(this.fetch_orders.bind(this), 10000);
+    }
   }
   componentDidMount() {
     this.fetch_orders();
